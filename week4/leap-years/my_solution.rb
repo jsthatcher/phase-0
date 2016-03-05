db2372d1d1088af8d22d3309258e4b63a88b5f14
Jack Thatcher <jsthatcher96@gmail.com>
2016-03-05 17:41:24 -0600

    Add 4.5 exersizes

---
+++ week4/leap-years/my_solution.rb
@@ -0,0 +1,15 @@
+# Leap Years
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def leap_year?(year)
+	if (year%4 == 0) and ((year%100 != 0) or (year%400 == 0))
+		return true
+	else
+		return false
+	end
+
+end
