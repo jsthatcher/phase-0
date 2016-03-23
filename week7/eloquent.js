29a568885e0fe6feb86fd164f76158daad533dd7
Jack Thatcher <jsthatcher96@gmail.com>
2016-03-23 13:15:00 -0500

    Add 7.2 questions UNCOMPLETE

---
+++ week7/eloquent.js
@@ -0,0 +1,29 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+
+// Program Structure
+// Write your own variable and do something to it.
+
+myFirstVariable = "It's a string!"
+
+console.log(myFirstVariable)
+
+prompt("What's your favorite food?")
+alert("Hey! That's mine too")
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+
+string = "#"
+while (string.length < 8) {
+ console.log(string)
+ string = string + "#"
+}
+
+// Functions
+
+// Complete the `minimum` exercise.
+
+
+
+// Data Structures: Objects and Arrays
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
